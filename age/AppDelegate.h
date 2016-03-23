129c81dbafeecfd39b29a3edf065b8be6a4ffd7f
千锋 <qianfeng@zhangdingguo.local>
2016-03-23 16:19:40 +0800

    我的项目

---
+++ age/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  age
+//
+//  Created by 千锋 on 16/3/23.
+//  Copyright (c) 2016年 张鼎国. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ age/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  age
+//
+//  Created by 千锋 on 16/3/23.
+//  Copyright (c) 2016年 张鼎国. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
